2a591efbec994f468dccf7dc73855aec1c8e18e3
Jason McCay <jason.mccay@gmail.com>
2014-12-26 23:52:10 -0600

    displaying articles in index view now

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,18 @@
 class ArticlesController < ApplicationController
 
 	def index
+
+		@articles = Article.order(updated_at: :desc).limit(25)
+
 	end
 
 	def show
 	end
 
 	def new 
+	end
+
+	def create
 	end
 
 
